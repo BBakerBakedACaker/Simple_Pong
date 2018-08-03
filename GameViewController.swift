51567f1d029a56006894a230b5cb356defd6682f
Brandon Baker <bbakerbakedacaker@gmail.com>
2018-08-03 14:31:52 -0500

    Add files via upload

---
+++ GameViewController.swift
@@ -0,0 +1,60 @@
+//
+//  GameViewController.swift
+//  Simple_Pong
+//
+//  Created by Brandon Baker on 8/3/18.
+//  Copyright © 2018 Brandon Baker. All rights reserved.
+//
+//
+//
+//  This is a standard Xcode created file. Only change that has been made is on lines 34-36.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            //Changed these to false so they would not display when running the project.
+            view.showsFPS = false
+            view.showsNodeCount = false
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
